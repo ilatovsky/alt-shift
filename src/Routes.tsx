2d22d2c1840c82bf3139850c89c0edf752dc1f40
Sasha Ilatovskii <ailatovsky@gmail.com>
2025-03-10 12:53:41 +0100

    remove ai scaffolded page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import { Switch, Route, Redirect, Router } from "wouter";
-import { ApplicationAi } from "./pages/application-ai";
 import { Application } from "./pages/application";
 import { ApplicationsList } from "./pages/applications-list";
 import { paths } from "./paths";
@@ -14,7 +13,6 @@ export const Routes: FC = () => (
         <Redirect to={paths.applications.list} />
       </Route>
       <Route path={paths.applications.list} component={ApplicationsList} />
-      <Route path={paths.applications.ai} component={ApplicationAi} />
       <Route path={paths.applications.view} component={Application} />
       <Route>404: No such page!</Route>
     </Switch>
