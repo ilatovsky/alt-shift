00a36f7900b92cddfd5c6e4a90d8254d340d808f
Sasha Ilatovskii <ailatovsky@gmail.com>
2025-03-10 12:26:12 +0100

    add code-splitting

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,22 +1,32 @@
-import { FC } from "react";
+import { FC, lazy, Suspense } from "react";
 import { Switch, Route, Redirect, Router } from "wouter";
-import { ApplicationAi } from "./pages/application-ai";
-import { Application } from "./pages/application";
-import { ApplicationsList } from "./pages/applications-list";
 import { paths } from "./paths";
 import { ScrollToTop } from "./components/ScrollToTop";
+
+// Lazy load components
+const Application = lazy(() =>
+  import("./pages/application").then((module) => ({
+    default: module.Application,
+  })),
+);
+const ApplicationsList = lazy(() =>
+  import("./pages/applications-list").then((module) => ({
+    default: module.ApplicationsList,
+  })),
+);
 
 export const Routes: FC = () => (
   <Router>
     <ScrollToTop />
-    <Switch>
-      <Route path="/">
-        <Redirect to={paths.applications.list} />
-      </Route>
-      <Route path={paths.applications.list} component={ApplicationsList} />
-      <Route path={paths.applications.ai} component={ApplicationAi} />
-      <Route path={paths.applications.view} component={Application} />
-      <Route>404: No such page!</Route>
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route path="/">
+          <Redirect to={paths.applications.list} />
+        </Route>
+        <Route path={paths.applications.list} component={ApplicationsList} />
+        <Route path={paths.applications.view} component={Application} />
+        <Route>404: No such page!</Route>
+      </Switch>
+    </Suspense>
   </Router>
 );
